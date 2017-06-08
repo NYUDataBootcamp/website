0afc2413675490cea34a41c72b68384b08efb4d2
Spencer Lyon <spencerlyon2@gmail.com>
2017-06-08 17:19:45 -0400

    ENH: updated deploy to build old versions of the site

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,16 @@
 #! /bin/bash
+# buld current version of the site first
+git checkout master
 git pull origin master
-hugo
+hugo -d public
+
+# then build old versions of the site
+git fetch origin
+git checkout fall2016
+hugo -d public/fall2016 -b http://nyu.data-bootcamp.com/fall2016
+
+# push updates
 ghp-import -n -p public
+
+# and checkout to master branch again
+git checkout master
