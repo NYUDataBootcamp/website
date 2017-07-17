9cc28613a8d55ae533f334328d4f0ca5554c6b56
Spencer Lyon <spencerlyon2@gmail.com>
2017-07-17 13:26:01 -0700

    ENH: enable https for fall2016 also

--- netlify_build.sh
+++ netlify_build.sh
@@ -2,4 +2,4 @@ hugo -d public
 git clone https://github.com/NYUDataBootcamp/website
 cd website
 git checkout fall2016
-hugo -d ../public/fall2016 -b http://nyu.data-bootcamp.com/fall2016
+hugo -d ../public/fall2016 -b https://nyu.data-bootcamp.com/fall2016
