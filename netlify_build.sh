eb139f660bf8ee29db04c79c9033734d4cfcbd5a
Spencer Lyon <spencerlyon2@gmail.com>
2018-01-15 12:05:51 -0500

    Only build tags that start with 'spring' or 'fall'

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,8 +1,14 @@
 hugo -d public
 git clone https://github.com/NYUDataBootcamp/website
 cd website
-for tag in `git tag`; do 
+for tag in `git tag -l "spring*"`; do 
 git checkout $tag
 hugo -d ../public/$tag -b https://nyu.data-bootcamp.com/$tag
 done
+
+for tag in `git tag -l "fall*"`; do 
+git checkout $tag
+hugo -d ../public/$tag -b https://nyu.data-bootcamp.com/$tag
+done
+
 git checkout master
