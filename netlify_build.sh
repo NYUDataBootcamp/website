3ce20f3572b4c1fc403881ba6f456ec5f0de15cc
Spencer Lyon <spencerlyon2@gmail.com>
2018-01-15 12:08:25 -0500

    add print message during tag build

--- netlify_build.sh
+++ netlify_build.sh
@@ -2,11 +2,13 @@ hugo -d public
 git clone https://github.com/NYUDataBootcamp/website
 cd website
 for tag in `git tag -l "spring*"`; do 
+echo "checking out tag: $tag"
 git checkout $tag
 hugo -d ../public/$tag -b https://nyu.data-bootcamp.com/$tag
 done
 
-for tag in `git tag -l "fall*"`; do 
+for tag in `git tag -l "fall*"`; do
+echo "checking out tag: $tag"
 git checkout $tag
 hugo -d ../public/$tag -b https://nyu.data-bootcamp.com/$tag
 done
