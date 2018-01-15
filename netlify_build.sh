94e0002b1d3107c0780e0782486ceff90d6c6121
Spencer Lyon <spencerlyon2@gmail.com>
2018-01-15 11:59:59 -0500

    ENH: build version of site for all tags

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,5 +1,8 @@
 hugo -d public
 git clone https://github.com/NYUDataBootcamp/website
 cd website
-git checkout fall2016
-hugo -d ../public/fall2016 -b https://nyu.data-bootcamp.com/fall2016
+for tag in `git tag`; do 
+git checkout $tag
+hugo -d ../public/$tag -b https://nyu.data-bootcamp.com/$tag
+done
+git checkout master
